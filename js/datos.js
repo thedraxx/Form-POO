7f34a088f2e43fe9c50668062300ab3007decd52
Frank <55867157+thedraxx@users.noreply.github.com>
2022-07-14 15:04:51 -0300

    mensaje error agregado

--- js/datos.js
+++ js/datos.js
@@ -9,11 +9,19 @@ class Person {
     greet() {
         return `Hola, ${this.usuario}`;
     }
+
+    verificacionCamposVacios() {
+        if (this.password == "" || this.usuario == "") return false;
+        return true;
+    }
+
 }
 
 
+const login = document.getElementById("login");
 
-function salida() {
+login.onsubmit = (e) => {
+    e.preventDefault();
 
     // Obtenemos los datos de la persona
     const password = document.getElementById("password").value;
@@ -21,6 +29,18 @@ function salida() {
 
     // llamamos a la funcion que crea el objeto
     const persona = new Person(password, usuario);
+
+    // Verificamos que los datos no esten vacios
+    const verificacion = persona.verificacionCamposVacios();
+
+    // Si los datos estan vacios, mostramos un mensaje de error
+    if (verificacion == false) {
+        document.getElementById("error").innerHTML = "Todos los campos son obligatorios";
+        setTimeout(() => {
+            document.getElementById("error").innerHTML = "";
+        }, 2000);
+    }
+
 
     // mostramos los datos de la persona
     console.log(persona.greet());
@@ -35,4 +55,10 @@ function salida() {
     // // Convertimos person a JSON
     // const personJSON = JSON.stringify(persona);
     // console.log(personJSON);
+
+
 }
+
+
+
+
